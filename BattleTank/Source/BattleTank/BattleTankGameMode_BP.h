85c91357a04fa4dd70d1dcc2d618141629216575
Ziggy Skull <saaggs@hotmail.com>
2018-12-22 22:56:43 -0800

    First commit after new Windows install

---
+++ BattleTank/Source/BattleTank/BattleTankGameMode_BP.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BattleTankGameMode_BP.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameMode_BP : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
