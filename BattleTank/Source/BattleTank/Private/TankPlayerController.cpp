da9e11a7f22e6a60a975cd84fb1c85281318826c
Ziggy Skull <saaggs@hotmail.com>
2018-12-29 13:45:11 -0800

    Tank Player Controller now Ticks every frame using a float, DeltaTime, DeltaSeconds

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,6 +4,9 @@
 #include "TankPlayerController.h"
 
 
+
+
+//called at the beginning
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -23,8 +26,28 @@ void ATankPlayerController::BeginPlay()
 }
 
 
+// Tick
+//called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	// Super
+	Super::Tick(DeltaTime);
+	// UE_LOG(LogTemp, Warning, TEXT("Player controller ticking"));
+	
+	AimTowardsCrosshair();
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,6 +19,11 @@ public:
 	
 	virtual void BeginPlay() override;
 
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
+
 };
 
 
