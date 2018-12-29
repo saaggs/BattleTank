05c6f21249fb57479dbfe0f804c74ab083f183c6
Ziggy Skull <saaggs@hotmail.com>
2018-12-29 13:13:00 -0800

    Get the Player Controller with C++ AI Tanks now recognize the Player's Tank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
+#include "TankPlayerController.h"
 #include "TankAIController.h"
 
 
@@ -11,7 +12,8 @@ void ATankAIController::BeginPlay()
 	Super::BeginPlay();
 
 	auto AIControlledTank = GetAIControlledTank();
-	
+	auto PlayerControlledTank = GetPlayerTank();
+
 	if (!AIControlledTank)
 	{
 		UE_LOG(LogTemp, Error, TEXT("AIController IS NOT possessing a tank"));
@@ -22,10 +24,27 @@ void ATankAIController::BeginPlay()
 	}
 
 	UE_LOG(LogTemp, Warning, TEXT("AIController Begin Play"));
+
+	if (!PlayerControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Cannot find Player Tank"));
+	}
+	if (GetPlayerTank())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("The PlayerControlledTank is: %s"), *(PlayerControlledTank->GetName()));
+	}
+
 }
 
 
 ATank* ATankAIController::GetAIControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,10 +14,12 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-public:
+private:
 
 	ATank* GetAIControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	ATank* GetPlayerTank() const;
 	
 };
